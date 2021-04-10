3adf33fafa437a7755bc91cb3fcb6000c6163247
juanwmedia <juan@wmedia.es>
2021-04-10 08:41:00 +0200

    Nuxt Auth install

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,26 @@
 export default {
-  target: 'static',
+  target: "static",
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     "@nuxtjs/tailwindcss",
   ],
   modules: [
     [
-      'storyblok-nuxt',
+      "storyblok-nuxt",
       {
-        accessToken: 'w2rZfyJCUykSb5BAMm4mAQtt',
-        cacheProvider: 'memory'
-      }
+        accessToken: "w2rZfyJCUykSb5BAMm4mAQtt",
+        cacheProvider: "memory",
+      },
     ],
-  ]
+    "@nuxtjs/axios",
+    "@nuxtjs/auth-next",
+  ],
+  auth: {
+    strategies: {
+      discord: {
+        clientId: "830170189576405024",
+        clientSecret: "G_Q02KdNot64LIb7WWvPxkbrYG-bdBUW",
+      },
+    },
+  },
 };
